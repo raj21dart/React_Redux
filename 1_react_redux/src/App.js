d311ddb3bc91893e9cc5f000b645b6a151c74628
Raj Sen <rajsen89611@gmail.com>
2021-09-09 11:55:35 +0000

    first

---
+++ 1_react_redux/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Redux</h1>
+      <h4>Using Redux</h4>
+
+      <div className="main">
+        <button>-</button>
+        <span>{`10`}</span>
+        <button>+</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
